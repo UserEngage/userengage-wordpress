edec1a9c678f02f4e19f3d7888e3cb61ae910b75
KarolMilewski <karol@civilhub.org>
2016-01-15 09:29:26 +0100

    Update Plugin

---
+++ readme.txt
@@ -0,0 +1,24 @@
+=== UserEngage.io Integration ===
+
+Contributors: UserEngage.io
+Tags: livechat, userengage, user engage, user engagement,
+Requires at least: 3.4.1
+Tested up to: 4.3.2
+Stable tag: 1.0
+License: GPLv2 or later
+
+== Description ==
+
+UserEngage.io Integration plugin enables you to add Livechat features to Your website.
+
+== Installation ==
+
+1. Upload the plugin files to the `/wp-content/plugins/userengage` directory, or install the plugin through the WordPress plugins screen directly.
+1. Activate the plugin through the 'Plugins' screen in WordPress
+1. Use the UserEngage.io screen to configure the plugin
+
+== Frequently Asked Questions ==
+
+= I need plugin support. Where should I ask questions? =
+
+https://userengage.io/ || contact@userengage.io
---
+++ userengage.php
@@ -0,0 +1,116 @@
+<?php
+
+/*
+Plugin Name: UserEngage.io Plugin
+Description: UserEngage.io Plugin for Wordpress.
+Version: 1.0
+Author: UserEngage
+Author URI: https://www.userengage.io
+License: GPLv2 or later
+*/
+/*  Copyright 2014  UserEngage.io
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+*/
+
+require_once('wp-updates-plugin.php');
+new WPUpdatesPluginUpdater_1401( 'http://wp-updates.com/api/2/plugin', plugin_basename(__FILE__));
+
+add_action('admin_head', 'ue__css');
+
+function ue__css() { ?>
+  <link rel="stylesheet" href="<?php  echo plugin_dir_url( __FILE__ ); ?>assets/css/style.css" type="text/css" media="all" />
+  <?php
+}
+
+function ue_widget ($meta) {
+
+	global $user_login , $user_email;
+      get_currentuserinfo();
+
+	$output = "
+	<script>
+		window.civchat = {
+			apiKey: \"$meta\",
+			name: \"$user_login\",
+			email: \"$user_email\"
+    	};
+	</script>" .
+	"<script src=\"https://widget.userengage.io/widget.js\"></script>";
+
+    echo $output;
+}
+
+
+if ( !class_exists( 'UserEngageScripts' ) ) {
+
+
+	class UserEngageScripts {
+
+		function UserEngageScripts() {
+			add_action( 'admin_init', array( &$this, 'admin_init' ) );
+			add_action( 'admin_menu', array( &$this, 'admin_menu' ) );
+			add_action( 'wp_head', array( &$this, 'wp_head' ) );
+		}
+
+		function admin_init() {
+			register_setting( 'ue-apiKey', 'ue__apiKey', 'trim' );
+		}
+
+		function admin_menu() {
+			add_menu_page(
+			'UserEngage.io' ,
+			'UserEngage.io',
+			'manage_options',
+			__FILE__, array( &$this, 'ue__panel' ),
+			'dashicons-admin-userengage'
+			);
+		}
+
+		function wp_head() {
+			$meta = get_option( 'ue__apiKey', '' );
+				if ( $meta != '' ) {
+					ue_widget($meta);
+				}
+		}
+
+		function ue__panel() { ?>
+			<div class="wrap">
+			<?php screen_icon(); ?>
+				<h2>UserEngage.io Plugin - Options</h2>
+				<hr />
+				<div class="ue__wrap">
+				<div class="ue__brand"></div>
+					<form name="dofollow" action="options.php" method="post">
+						<?php settings_fields( 'ue-apiKey' ); ?>
+						<h3 for="ue__apiKey">Your Api Key:</h3>
+						<input type="text" id="apiKey" name="ue__apiKey" value="<?php echo esc_html( get_option( 'ue__apiKey' ) ); ?>" required><br>
+						Please enter your application key which has been sent to your email address.<br>
+						The api key is a 64 letter and number key.
+						<p class="submit">
+							<input class="button button-primary" type="submit" name="Submit" value="Submit Api Key" />
+						</p>
+					</form>
+				</div>
+			</div>
+		<?php
+		}
+	}
+
+$userengage_scripts = new UserEngageScripts();
+
+}
+
+
