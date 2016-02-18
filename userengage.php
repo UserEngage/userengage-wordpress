f20fc5a7589d0d6ff84cd7d64965beccbfb46994
KarolMilewski <karol@civilhub.org>
2016-02-18 17:05:24 +0100

    Update names

--- userengage.php
+++ userengage.php
@@ -8,7 +8,7 @@ Author: UserEngage
 Author URI: https://www.userengage.io
 License: GPLv2 or later
 */
-/*	Copyright 2014 UserEngage
+/*	Copyright 2015 UserEngage
 
 	This program is free software; you can redistribute it and/or modify
 	it under the terms of the GNU General Public License, version 2, as
@@ -26,14 +26,14 @@ License: GPLv2 or later
 */
 
 
-function ue_admin_style() {
-	wp_register_style( 'custom_wp_admin_css', plugin_dir_url( __FILE__ ) . 'assets/css/style.css', false, '1.0.0' );
-	wp_enqueue_style( 'custom_wp_admin_css' );
+function UserEngageScript_admin_style() {
+	wp_register_style( 'UserEngageScript_wp_admin_css', plugin_dir_url( __FILE__ ) . 'assets/css/style.css', false, '1.0.0' );
+	wp_enqueue_style( 'UserEngageScript_wp_admin_css' );
 }
-add_action( 'admin_enqueue_scripts', 'ue_admin_style' );
+add_action( 'admin_enqueue_scripts', 'UserEngageScript_admin_style' );
 
 
-function ue_widget ($meta) {
+function UserEngageScript_widget ($meta) {
 
 	global $user_login , $user_email;
 	get_currentuserinfo();
@@ -50,11 +50,11 @@ function ue_widget ($meta) {
 	echo $output;
 }
 
-function ue_widget_js() {
+function UserEngageScript_widget_js() {
 	wp_enqueue_script( 'UserEngage', 'https://widget.userengage.io/widget.js', array(), null, true );
 }
 
-add_action( 'wp_enqueue_scripts', 'ue_widget_js' );
+add_action( 'wp_enqueue_scripts', 'UserEngageScript_widget_js' );
 
 if ( !class_exists( 'UserEngageScripts' ) ) {
 
@@ -62,43 +62,43 @@ if ( !class_exists( 'UserEngageScripts' ) ) {
 	class UserEngageScripts {
 
 		function UserEngageScripts() {
-			add_action( 'admin_init', array( &$this, 'ue_admin_init' ) );
-			add_action( 'admin_menu', array( &$this, 'ue_admin_menu' ) );
-			add_action( 'wp_head', array( &$this, 'ue_wp_head' ) );
+			add_action( 'admin_init', array( &$this, 'UserEngageScript_admin_init' ) );
+			add_action( 'admin_menu', array( &$this, 'UserEngageScript_admin_menu' ) );
+			add_action( 'wp_head', array( &$this, 'UserEngageScript_wp_head' ) );
 		}
 
-		function ue_admin_init() {
-			register_setting( 'ue-apiKey', 'ue__apiKey', 'trim' );
+		function UserEngageScript_admin_init() {
+			register_setting( 'UserEngageScript-apiKey', 'UserEngageScript__apiKey', 'trim' );
 		}
 
-		function ue_admin_menu() {
+		function UserEngageScript_admin_menu() {
 			add_menu_page(
 			'UserEngage.io',
 			'UserEngage.io',
 			'manage_options',
-			__FILE__, array( &$this, 'ue__panel' ),
+			__FILE__, array( &$this, 'UserEngageScript__panel' ),
 			'dashicons-admin-userengage'
 			);
 		}
 
-		function ue_wp_head() {
-			$meta = get_option( 'ue__apiKey', '' );
+		function UserEngageScript_wp_head() {
+			$meta = get_option( 'UserEngageScript__apiKey', '' );
 				if ( $meta != '' ) {
-					ue_widget($meta);
+					UserEngageScript_widget($meta);
 				}
 		}
 
-		function ue__panel() { ?>
+		function UserEngageScript__panel() { ?>
 			<div class="wrap">
 			<?php screen_icon(); ?>
 				<h2>UserEngage.io Plugin - Options</h2>
 				<hr />
-				<div class="ue__wrap">
-				<div class="ue__brand"></div>
+				<div class="UserEngageScript__wrap">
+				<div class="UserEngageScript__brand"></div>
 					<form name="dofollow" action="options.php" method="post">
-						<?php settings_fields( 'ue-apiKey' ); ?>
-						<h3 for="ue__apiKey">Your Api Key:</h3>
-						<input type="text" id="apiKey" name="ue__apiKey" value="<?php echo esc_html( get_option( 'ue__apiKey' ) ); ?>" required><br>
+						<?php settings_fields( 'UserEngageScript-apiKey' ); ?>
+						<h3 for="UserEngageScript__apiKey">Your Api Key:</h3>
+						<input type="text" id="apiKey" name="UserEngageScript__apiKey" value="<?php echo esc_html( get_option( 'UserEngageScript__apiKey' ) ); ?>" required><br>
 						Please enter your application key which has been sent to your email address.<br>
 						The api key is a 64 letter and number key.
 						<p class="submit">
